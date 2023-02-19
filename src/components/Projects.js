ca59f53f7ced66e3342ce97b26ba0ece56951c5a
Lisa Walsh <lwalshphotography@yahoo.com>
2023-02-19 12:28:32 -0500

    trying to fix styling

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,10 +1,31 @@
 function Projects(){
     return (
-        <div>
-            <p>
-                My Projects
-            </p>
+        <>
+            <section className="services">
+        <div class="container">
+        <h1 class="services-head">Services</h1>
+            <div class="card-grid">
+                <div class="card">
+        
+                <h2>Graphic Design</h2>
+                <p>Coming Soon</p>
+                </div>
+
+                <div class="card">
+        
+                <h2>Web Development</h2>
+                <p><a href="https://github.com/Lwalsh2022" target="_blank">Github</a></p>
+                </div>
+
+                <div class="card">
+        
+                <h2>Photography</h2>
+                <p><a href="https://fineartamerica.com/profiles/2-lisa-walsh" target="_blank">View Portfolio</a></p>
+                </div>
+            </div>
         </div>
+        </section>
+    </>
     )
 }
 export default Projects;
